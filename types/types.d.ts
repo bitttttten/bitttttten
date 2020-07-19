3e026dc2632de8adf584f2f39638d82eb0daf93d
bitttttten <bitttttten@googlemail.com>
2020-07-19 15:53:05 +0200

    add in types to get recently loved

--- types/types.d.ts
+++ types/types.d.ts
@@ -24,3 +24,83 @@ export interface LocalArtist {
 	image: string
 	url: string
 }
+
+export interface ExternalUrls {
+	spotify: string
+}
+
+export interface AlbumArtist {
+	external_urls: ExternalUrls
+	href: string
+	id: string
+	name: string
+	type: string
+	uri: string
+}
+
+export interface SavedTracksAlbumExternalUrls {
+	spotify: string
+}
+
+export interface AlbumImage {
+	height: number
+	url: string
+	width: number
+}
+
+export interface Album {
+	album_type: string
+	artists: AlbumArtist[]
+	available_markets: string[]
+	external_urls: SavedTracksAlbumExternalUrls
+	href: string
+	id: string
+	images: AlbumImage[]
+	name: string
+	type: string
+	uri: string
+}
+
+export interface SavedTracksArtistExternalUrls {
+	spotify: string
+}
+
+export interface SavedTracksArtist {
+	external_urls: SavedTracksArtistExternalUrls
+	href: string
+	id: string
+	name: string
+	type: string
+	uri: string
+}
+
+export interface SavedTracksExternalUrls {
+	spotify: string
+}
+
+export interface Track {
+	album: Album
+	artists: SavedTracksArtist[]
+	available_markets: string[]
+	disc_number: number
+	duration_ms: number
+	explicit: boolean
+	external_urls: SavedTracksExternalUrls
+	name: string
+	uri: string
+}
+
+export interface SavedTracksItem {
+	added_at: Date
+	track: Track
+}
+
+export interface SavedTracksResponse {
+	href: string
+	items: SavedTracksItem[]
+	limit: number
+	next: string
+	offset: number
+	previous?: any
+	total: number
+}
